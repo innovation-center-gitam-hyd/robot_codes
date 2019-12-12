e8f0f8ab19808566f303f4d22976ccca3658c162
Srivastav Nanduri <srivastav.nanduri.98@gmail.com>
2019-12-12 15:07:31 +0530

    Changed Express Code

--- express.js
+++ express.js
@@ -15,6 +15,10 @@ app.use(express.urlencoded({
 
 board.on('ready', () => {
     var led = five.Led(13);
+    var in1 = new Led(7)
+    var in2 = new Led(8)
+    var in3 = new Led(9)
+    var in4 = new Led(10)
     app.get("/", (req, res) => {
         res.status(200).sendFile(path.join(__dirname+'/views/index.html'))
     });
@@ -24,30 +28,50 @@ board.on('ready', () => {
         console.log(direction)
         if(direction == 'forward'){
             led.blink(1000);
+            in1.on();
+            in2.off();
+            in3.on();
+            in4.off();
             res.status(200).json({
                 direction: direction
             })
         }
         else if(direction == 'backward'){
             led.blink(2000);
+            in1.off();
+            in2.on();
+            in3.off();
+            in4.on();
             res.status(200).json({
                 direction: direction
             })
         }
         else if(direction == 'stop'){
             led.blink(10000);
+            in1.off();
+            in2.off();
+            in3.off();
+            in4.off();
             res.status(200).json({
                 direction: direction
             })
         }
         else if(direction == 'right'){
             led.blink(500);
+            in1.on();
+            in2.off();
+            in3.off();
+            in4.on();
             res.status(200).json({
                 direction: direction
             })
         }
         else if(direction == 'left'){
             led.blink(5000);
+            in1.off();
+            in2.on();
+            in3.on();
+            in4.off();
             res.status(200).json({
                 direction: direction
             })
